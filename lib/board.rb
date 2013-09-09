577d8ef4d17046a4f3ab4f0882bfbf00746103fe
xiaoting wright <haley.xt.wright@gmail.com>
2013-09-09 17:02:54 +0100

    Added spec requirements

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,10 @@
 class Board
+
+	attr_reader :player_board
+
   def initialize player
    	@player = player
+   	@player_board = rows_hash(@player.board)
   end
 
   def owner
@@ -8,18 +12,31 @@ class Board
   end
 
   def register_shot at_coordinates
-    @shot_coordinates = at_coordinates
+    @shot_coordinates = at_coordinates.chars
+   
+    @player_board[@shot_coordinates[0]][@shot_coordinates[1].to_i-1] == 's' ? @result = "hit" : @result = "water" 
+    @player_board[@shot_coordinates[0]][@shot_coordinates[1].to_i-1] = 'x'
+
+    @result
 	end
 
 	def rows
-		@rows = []
+    @player_board.values
+  end
 
-		10.times {@rows << ['','','','','','','','','','']}
-    @rows
+  private
 
-    arr = %w{A,B,C,D,E,F,G,H,I,J}
+  def top_column
+    @top_column = %w{A B C D E F G H I J}
+  end
 
-    
-	end
+  def rows_hash player_board
+    # rows = player_board
+    # index = top_column
+    @player_board = {}
+    i = 0
+    top_column.each {|key| @player_board[key] = player_board[i]; i += 1 }
+    @player_board 
+  end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,29 +1,32 @@
 require_relative '../lib/board'
 
 describe Board do 
+
+	let(:player) {double :player, name: 'Ting', board: [['','','','','','','','','',''], ['s','','','','','','','','',''],['','','','','','','','','',''],['','','','','','','','','',''],['','','','','','','','','',''],['','','','','','','','','',''], ['s','','','','','','','','',''],['','','','','','','','','',''],['','','','','','','','','',''],['','','','','','','','','','']]}
+  let(:board) {Board.new player}
+
   it 'has an owner' do 
-		player = double :player, name: 'jodie'
     board = Board.new player
-		expect(board.owner).to eq 'jodie'
+		expect(board.owner).to eq 'Ting'
 	end
 
 	it 'registers the shot at the coodinates passed - B1' do
-    player = double :player
-    board = Board.new player
-    expect(board.register_shot('B1')).to eq 'B1'
+    expect(board.register_shot('B1')).to eq 'hit'
 	end 
 
 	it 'returns an array containing 10 arrays' do 
-    player = double :player
+    # player = double :player
     board = Board.new player
     expect(board.rows.length).to eq 10 
 	end
 
 	it 'returns an array containing 10 arrays with 10 elements' do 
-    player = double :player
+    # player = double :player
     board = Board.new player
     expect(board.rows[0].length).to eq 10 
 	end
 
 	it 'returns an array containing 10 arrays with 10 elements '
+
+	it 'can convert arrays to hash' 
 end
